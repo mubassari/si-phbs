87211297d09b2a004c39b7d4e0b4e0c0dd665ab2
mubassari <maulimubassari23@gmail.com>
2023-03-16 19:40:04 +0800

    menambahkan Bahasa Indonesia

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <title>{{ config('app.name') }}</title>
 
         <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
@@ -14,7 +14,7 @@
     <body class="{{ $class  }}">
 
         @yield('main')
-        
+
         <!-- jQuery -->
         <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
         <!-- Bootstrap 4 -->
