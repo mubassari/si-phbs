61be6468023227e2a967e91219a1ced8a34b42c0
mubassari <maulimubassari23@gmail.com>
2023-03-24 23:02:52 +0800

    menyelesaikan #4

--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -1,1 +1,18 @@
 @extends('layouts.app', ['breadcrumb' => 'List Members'])
+@section('content')
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3">
+            @foreach ($list_indikator as $indikator)
+                <div class="col mb-4">
+                    <div class="card">
+                        <img src="{{ $indikator->path_foto }}" class="card-img-top" alt="{{ $indikator->judul }}">
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bolder py-3 text-capitalize">{{ $indikator->judul }}</h5>
+                            <p class="card-text">{{ $indikator->isi }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
