35dc0cb0ad526f2f2a67aa5932d10fe7fd31f778
c-syamsulhadi-si <c.syamsulhadi.si@gmail.com>
2023-03-29 05:22:40 +0800

    Menambahkan inputan text  catatatan admin

--- resources/views/pages/user/status.blade.php
+++ resources/views/pages/user/status.blade.php
@@ -15,6 +15,7 @@
                     <x-forms.radio-status label="Status Verifikasi" name="status_verifikasi" itemValue="{{ $user->status_verifikasi }}"/>
                     <x-forms.radio-status label="Status Partisipasi" name="status_pertisipasi" itemValue="{{ $user->status_partisipasi }}"/>
                     <x-forms.radio-status label="Status Draft" name="status_draft" itemValue="{{ $user->status_draft }}"/>
+                    <x-forms.input-group label="Tambahkan Catatan" name="catatan_admin" id="catatan_admin" type="text" itemValue="{{ $user->catatan_admin }}" />
                     
                         <a class="btn btn-warning text-white" href="{{ route('user.index') }}">Kembali</a>
                     <button type="submit" class="btn btn-primary">Simpan</button>
