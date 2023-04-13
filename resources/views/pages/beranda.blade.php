6184d3a19edd320a2610c589c5dfbbbb857f1211
c-syamsulhadi-si <c.syamsulhadi.si@gmail.com>
2023-04-13 15:23:22 +0800

    Menyesuakian tampilan isi indikator pada menu beranda

--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -3,12 +3,12 @@
     <div class="container">
         <div class="row row-cols-1 row-cols-md-3">
             @foreach ($list_indikator as $indikator)
-                <div class="col mb-4">
+                <div class="col-md-4 mb-4">
                     <div class="card">
                         <img src="{{ $indikator->path_foto }}" class="card-img-top" alt="{{ $indikator->judul }}">
                         <div class="card-body">
-                            <h5 class="card-title font-weight-bolder py-3 text-capitalize">{{ $indikator->judul }}</h5>
-                            <p class="card-text">{!! $indikator->isi !!}</p>
+                            <h5 class="card-title font-weight-bolder py-3 text-capitalize">{{ $indikator->judul }}</h5> <br> <br>
+                            {!! Str::limit($indikator->isi, 250, '...') !!} @if (Str::length($indikator->isi) > 1) <a href="">Baca selengkapnya</a> @endif
                         </div>
                     </div>
                 </div>
