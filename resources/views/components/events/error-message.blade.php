0eee4e933833c5dcca76e9467012c4fcbff5659e
mubassari <maulimubassari23@gmail.com>
2023-04-16 08:34:45 +0800

    otomatis menghapus pesan error saat form update

--- resources/views/components/events/error-message.blade.php
+++ resources/views/components/events/error-message.blade.php
@@ -1,3 +1,13 @@
 @error($error)
     <div role="alert" class="invalid-feedback">{{ $message }}</div>
 @enderror
+
+@push('script')
+    <script>
+        $('form').on('change', function() {
+            // Remove any validation errors
+            $(this).find('.is-invalid').removeClass('is-invalid');
+            $(this).find('.invalid-feedback').remove();
+        });
+    </script>
+@endpush
